ba7f6c1c5061ad9fa3612c68fb751c49a40827c3
Leonardo Nakao <leonardo@code49.net>
2020-08-06 23:32:24 -0300

    test: fix params

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -5,8 +5,8 @@ describe('SignUp Controller', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
-        name: 'any_name',
         email: 'any_email@mail.com',
+        password: 'any_password',
         passwordConfirmation: 'any_password'
       }
     }
